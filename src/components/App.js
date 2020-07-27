9d035b48e0f7c83e2997152f37335a94fcc34117
Tripple-A <biodun9@gmail.com>
2020-07-27 08:48:13 +0100

    added header to App.js

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+        <div class="row bg-dark text-light">
+            <div class="col-12">
+                <h1 class="text-center py-3">
+                    <span class="display-3">The Pokemon League</span>
+            </h1>
+
+        </div>
+        </div>
+);
+
+export default Header;
