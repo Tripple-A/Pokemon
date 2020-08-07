9f8612f589c26bcbd5cae8a4951c69b5b8b695b3
Tripple-A <biodun9@gmail.com>
2020-08-07 15:51:06 +0100

    replaced createStore with configureStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import './styles/index.css';
